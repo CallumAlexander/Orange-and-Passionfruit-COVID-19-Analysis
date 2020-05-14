a2b2a412d8a57b64f46f2511155e256bd9e46641
Cal Alexander <s1931801@ed.ac.uk>
2020-05-14 21:08:02 +0100

    add: Basic processing displaying of the data using methods
    Signed-off-by: Cal Alexander <s1931801@ed.ac.uk>

--- Handling.py
+++ Handling.py
@@ -2,17 +2,29 @@
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt#
+import matplotlib.pyplot as plt  #
+
+csv = 'Hospitalization_all_locs.csv'
 
 
-df = pd.read_csv('Hospitalization_all_locs.csv')
-is_UK = df['location_name'] == 'United Kingdom'
-df = df[is_UK]
-df = df.iloc[:, [1, 2, 12]]
-df.set_index('date')
+def handle(csv):
+    df = pd.read_csv(csv)
+    is_UK = df['location_name'] == 'United Kingdom'
+    df = df[is_UK]
+    df = df.iloc[:, [2, 12]]
+    df.set_index('date')
+    return df
 
-fig, ax = plt.subplots()
-ax.plot(df.iloc[:, 1], df.iloc[:, 2])
-plt.show()
 
-print('\nDone')
+def display(df):
+    fig, ax = plt.subplots()
+    ax.plot(df.iloc[:, 0], df.iloc[:, 1])
+    ax.grid()
+    plt.show()
+
+
+if __name__ == '__main__':
+    df = handle(csv)
+    display(df)
+
+    print('\nDone')
